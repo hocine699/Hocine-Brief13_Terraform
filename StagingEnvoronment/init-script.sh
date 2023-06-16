f34f19e11cc4b5b4d97e31b125cd0a374cb23865
hocine699 <127848706+hocine699@users.noreply.github.com>
2023-06-16 12:26:06 +0200

    Update init-script.sh

--- StagingEnvoronment/init-script.sh
+++ StagingEnvoronment/init-script.sh
@@ -5,4 +5,4 @@ sudo apt install curl
 sudo curl -fsSL https://get.docker.com -o get-docker.sh
 sudo sh get-docker.sh
 #Creating container
-sudo docker run -d -p 80:80 --name hocine hocinho699/hocine-nginx
+sudo docker run -d -p 80:80 --name hocine hocinho699/hocine-brief14
