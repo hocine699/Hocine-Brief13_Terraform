90a5bbf690eefc9efecb4a2fab9a6bb1b6a60a97
hocine699 <127848706+hocine699@users.noreply.github.com>
2023-06-16 16:38:10 +0200

    Update init-script.sh

--- ProdEnvironment/init-script.sh
+++ ProdEnvironment/init-script.sh
@@ -5,4 +5,4 @@ sudo apt install curl
 sudo curl -fsSL https://get.docker.com -o get-docker.sh
 sudo sh get-docker.sh
 #Creating container
-sudo docker run -d -p 80:1234 --name brief14 hocinho699/brief-14
+sudo docker run -d -p 80:1234 --name brief14 hocinho699/brief14
